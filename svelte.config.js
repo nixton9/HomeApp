6b22a600a1f56a60acdff490a469ecb498d38878
Eduardo <eduardojoaoaraujo@gmail.com>
2022-02-15 17:41:37 +0000

    Added vercel adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,6 @@ const config = {
 	kit: {
 		adapter: vercel(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
