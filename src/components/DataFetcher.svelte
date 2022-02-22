c987f300594636b654fa0f60d68a793ce3237f53
Eduardo <eduardojoaoaraujo@gmail.com>
2022-02-22 08:44:23 +0000

    Changed devices request

--- src/components/DataFetcher.svelte
+++ src/components/DataFetcher.svelte
@@ -2,34 +2,38 @@
 	import { onMount } from 'svelte'
 	import { devices, isLoadingDevices } from '$stores/devices'
 	import { shapesDevice } from '$stores/shapes'
-	import type { Device } from '$lib/types.d'
 
 	let shapesAddress: string
 	shapesDevice.subscribe((newDevice) => (shapesAddress = newDevice))
 
 	onMount(async () => {
-		isLoadingDevices.set(true)
+		const SSDevices = JSON.parse(window.sessionStorage.getItem('devices'))
+		if (SSDevices && SSDevices.length > 0) {
+			devices.set(SSDevices)
+		} else {
+			isLoadingDevices.set(true)
 
-		const goveeResponse = await fetch('/api/govee')
-		const nanoleafResponse = await fetch('/api/nanoleaf', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				name: 'Shapes',
-				ip: shapesAddress
+			const goveeResponse = await fetch('/api/govee')
+			const nanoleafResponse = await fetch('/api/nanoleaf', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: 'Shapes',
+					ip: shapesAddress
+				})
 			})
-		})
 
-		const goveeData = await goveeResponse.json()
-		const nanoleafData = await nanoleafResponse.json()
-		const allDevices = [
-			...(goveeData.data && goveeData.data),
-			...(nanoleafData.data && nanoleafData.data)
-		]
+			const goveeData = await goveeResponse.json()
+			const nanoleafData = await nanoleafResponse.json()
+			const allDevices = [
+				...(goveeData.data && goveeData.data),
+				...(nanoleafData.data && nanoleafData.data)
+			]
 
-		devices.set(allDevices)
-		isLoadingDevices.set(false)
+			devices.set(allDevices)
+			isLoadingDevices.set(false)
+		}
 	})
 </script>
--- src/stores/devices.ts
+++ src/stores/devices.ts
@@ -1,4 +1,12 @@
 import { writable } from 'svelte/store'
+import { browser } from '$app/env'
 
 export const devices = writable([])
-export const isLoadingDevices = writable(true)
+export const isLoadingDevices = writable(false)
+
+devices.subscribe((newDevices) => {
+	console.log('new', newDevices)
+	if (browser) {
+		window.sessionStorage.setItem('devices', JSON.stringify(newDevices))
+	}
+})
